9c25215e19a339ea0d9dd9537c9724199481f83d
FlorianCahay <38866797+FlorianCahay@users.noreply.github.com>
2018-12-25 20:09:37 +0100

    Ajout classe Rationnel (contenu)

--- src/Types/Rationnel.java
+++ src/Types/Rationnel.java
@@ -1,4 +1,72 @@
 package Types;
 
+import java.math.BigInteger;
+
 public class Rationnel {
+    private final BigInteger numerateur;
+    private final BigInteger denominateur;
+
+    public BigInteger getDenominateur() {
+        return denominateur;
+    }
+
+    public BigInteger getNumerateur() {
+        return numerateur;
+    }
+
+    public Rationnel(BigInteger numerateur, BigInteger denominateur) {
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+    }
+
+    /*
+        Multiplie deux rationnels : a*b
+     */
+    public Rationnel mul(Rationnel a, Rationnel b) {
+        return new Rationnel(
+                a.getNumerateur().multiply(b.getNumerateur()),
+                a.getDenominateur().multiply(b.getDenominateur())
+        );
+    }
+
+    /*
+        Additionne deux rationnels : a+b
+     */
+    public Rationnel add(Rationnel a, Rationnel b) {
+        if (a.getDenominateur() != b.getDenominateur()) {
+            return new Rationnel(
+                    (a.getNumerateur().multiply(b.getDenominateur())).add(a.getDenominateur().multiply(b.getNumerateur())),
+                    a.getNumerateur().multiply(b.getDenominateur())
+            );
+        } else {
+            return new Rationnel(
+                    a.getNumerateur().add(b.getNumerateur()),
+                    a.getDenominateur()
+            );
+        }
+
+    }
+    /*
+        Soustrait deux rationnels : a-b
+     */
+    public Rationnel sub(Rationnel a, Rationnel b) {
+        if (a.getDenominateur() != b.getDenominateur()) {
+            return new Rationnel(
+                    (a.getNumerateur().multiply(b.getDenominateur())).subtract(a.getDenominateur().multiply(b.getNumerateur())),
+                    a.getNumerateur().multiply(b.getDenominateur())
+            );
+        } else {
+            return new Rationnel(
+                    a.getNumerateur().subtract(b.getNumerateur()),
+                    a.getDenominateur()
+            );
+        }
+    }
+
+    /*
+        Divise deux rationnels : a/b
+     */
+    public Rationnel div(Rationnel a, Rationnel b) {
+        return mul(a, new Rationnel(b.getDenominateur(), b.getNumerateur()));
+    }
 }
