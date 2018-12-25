32b2a270658dab414a04a09f933b70180c53c24b
FlorianCahay <38866797+FlorianCahay@users.noreply.github.com>
2018-12-25 21:44:10 +0100

    Début de la structure du projet

---
+++ src/Console/Console.java
@@ -0,0 +1,24 @@
+package Console;
+
+import java.util.Scanner;
+
+public class Console {
+    private Scanner scanner;
+
+    public Console() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void lecture() {
+
+    }
+
+    public void evaluation() {
+
+    }
+
+    public void affichage() {
+
+    }
+
+}
